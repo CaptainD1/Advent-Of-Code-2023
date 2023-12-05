8dd97660b2552739a812736c90c3a4c8b9be76f2
Captain_D1 <legohavend@gmail.com>
2023-12-05 18:04:29 -0500

    Completed day 3 part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,3 +1,4 @@
+use std::str::from_utf8;
 use std::{fs, cmp};
 use std::time::SystemTime;
 
@@ -15,7 +16,7 @@ struct Args {
     timing: bool,
 
     /// How many loops to run when measuring timing
-    #[arg(long, default_value_t = 1000)]
+    #[arg(short, long, default_value_t = 1000)]
     loops: usize,
 
     /// Display debug info
@@ -38,70 +39,98 @@ fn main() {
             let duration = (now.elapsed().unwrap().as_nanos() as f64 / args.loops as f64) / 1e6;
             println!("Total run time: {:.4}ms (after {} loops)", duration, args.loops);
         } else {
-            println!("The answer is: {}", calculate(&contents, args.debug));
+            let (part_sum, gear_sum) = calculate(&contents, args.debug);
+            if args.debug {
+                println!();
+            }
+            println!("The part sum is: {}", part_sum);
+            println!("The gear sum is: {}", gear_sum);
         }
     } else {
         println!("Error opening file '{}'", &args.filename);
     }
 }
 
-fn calculate(contents: &Vec<u8>, debug: bool) -> u32 {
+fn calculate(contents: &Vec<u8>, debug: bool) -> (usize, usize) {
     let lines: Vec<&[u8]> = contents.split(|num| *num == b'\n').filter(|line| line.len() > 1).collect();
-    let mut start_num = None;
-    let mut sum = 0;
+
+    let mut part_sum = 0;
+    let mut gear_sum = 0;
+
+    let mut total = 0;
+    
     for (y, line) in lines.iter().enumerate() {
         for (x, ch) in line.iter().enumerate() {
-            // Is a digit
-            if *ch >= b'0' && *ch <= b'9' {
-                if start_num.is_none() {
-                    start_num = Some((x, y));
-                }
-            } else {
-                if let Some(start_num) = start_num.take() {
-                    if let Some(part_id) = get_part_num(&lines, start_num, (x, y), debug) {
-                        sum += part_id;
-                    }
-                }
-            }
-        }
-        if let Some(start_num) = start_num.take() {
-            if let Some(part_id) = get_part_num(&lines, start_num, (lines[y].len(), y), debug) {
-                sum += part_id;
-            }
-        }
-    }
-    sum
-}
-
-fn get_part_num(lines: &Vec<&[u8]>, start: (usize, usize), end: (usize, usize), debug: bool) -> Option<u32> {
-    let start_corner = (start.0.checked_sub(1).unwrap_or(0),
-            start.1.checked_sub(1).unwrap_or(0));
-            // Assuming all lines are of equal length (which they are)
-    let end_corner = (cmp::min(end.0 + 1, lines.len()),
-            cmp::min(end.1 + 2, lines.len()));
-
-    let mut is_part_num = false;
-
-    'outer: for y in start_corner.1..end_corner.1 {
-        for x in start_corner.0..cmp::min(end_corner.0, lines[y].len()) {
-            match lines[y][x] {
-                b'0'..=b'9' | b'.' => {},
-                _ => {
-                    is_part_num = true;
-                    break 'outer;
+            // Is a part
+            if !is_digit(*ch) && !(*ch == b'.') {
+                let nums = get_part_nums(&lines, x, y, debug);
+                total += nums.len();
+                part_sum += nums.iter().sum::<usize>();
+                if *ch == b'*' && nums.len() == 2 {
+                    gear_sum += nums[0] * nums[1];
                 }
             }
         }
     }
     if debug {
-        println!("Number cutout: ({}, {}) -> ({}, {})", start_corner.0, start_corner.1, end_corner.0, end_corner.1);
-        println!("Is part num: {}", is_part_num);
-        println!("Number: {}", std::str::from_utf8(&lines[start.1][start.0..end.0]).unwrap());
-        println!("{}\n", lines[start_corner.1..end_corner.1].iter().map(|line| std::str::from_utf8(&line[start_corner.0..end_corner.0]).unwrap()).collect::<Vec<&str>>().join("\n"));
+        println!("Total part numbers found: {}", total);
     }
-    if is_part_num {
-        Some(std::str::from_utf8(&lines[start.1][start.0..end.0]).unwrap().parse().unwrap())
+    (part_sum, gear_sum)
+}
+
+fn get_part_nums(lines: &Vec<&[u8]>, x_pos: usize, y_pos: usize, debug: bool) -> Vec<usize> {
+    let x_start = x_pos.checked_sub(1).unwrap_or(0);
+    let x_end = cmp::min(x_pos + 2, lines[0].len());
+    let y_start = y_pos.checked_sub(1).unwrap_or(0);
+    let y_end = cmp::min(y_pos + 2, lines.len());
+
+    let mut in_num = false;
+    let mut nums: Vec<usize> = Vec::new();
+
+    if debug {
+        println!("\nCenter pos: ({}, {})", x_pos, y_pos);
+        println!("{}", lines[y_start..y_end].iter().map(|line| std::str::from_utf8(&line[x_start..x_end]).unwrap()).collect::<Vec<&str>>().join("\n"));
+    }
+
+    for y in y_start..y_end {
+        for x in x_start..x_end {
+            let ch = lines[y][x];
+            if is_digit(ch){
+                if !in_num {
+                    in_num = true;
+                    nums.push(get_full_number(lines, x, y, debug));
+                }
+            } else if in_num { // Was searching through a number but no longer in one
+                in_num = false;
+            }
+        }
+        in_num = false;
+    }
+    nums
+}
+
+fn get_full_number(lines: &Vec<&[u8]>, x_pos: usize, y_pos: usize, debug: bool) -> usize {
+    let mut x = x_pos.checked_sub(1).unwrap_or(0);
+    while is_digit(lines[y_pos][x]) && x > 0 {
+        x -= 1;
+    }
+    // Prevent issues with negative usize
+    let x_start = if is_digit(lines[y_pos][x]) {
+        x
     } else {
-        None
+        x + 1
+    };
+    x = x_pos + 1;
+    while x < lines[y_pos].len() && is_digit(lines[y_pos][x]) {
+        x += 1;
     }
+    let num = from_utf8(&lines[y_pos][x_start..x]).unwrap().parse().unwrap();
+    if debug {
+        println!("{} @ ({}, {}) -> ({}, {})", num, x_start, y_pos, x, y_pos);
+    }
+    num
+}
+
+fn is_digit(ch: u8) -> bool {
+    ch >= b'0' && ch <= b'9'
 }
